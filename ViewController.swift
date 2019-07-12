ac69466cc9c01d73fd0f9060471cd75c5cbef505
Apple <apple@Apples-MacBook-Pro-7.local>
2019-07-12 16:11:24 -0500

    Iteration 2

--- ViewController.swift
+++ ViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let Emoji = ["🤠": "YEE HAWWWWW", "🙌": "GET ALONG LITTLE DOGGIE","🏇": "I LASSO YOU!", "📦":"FOUR SHIFTER GEARBOX","🍝":"A BOWL OF SPAGHETTI"]
+    let customMessage =
+        ["YEE HAWWWWW":[ "YEE HAWWWWW", "YEE HAW"],
+        "GET ALONG LITTLE DOGGIE":["GET OFF MY PORCH","GET OFF MY PORCH!"],
+        "I LASSO YOU!":["YEE HAW","I LASSO YOU BUCKAROO!"],
+        "FOUR SHIFTER GEARBOX":["ONE TWO THREE FOUR","ONE TWO THREE FOUR!"],
+        "A BOWL OF SPAGHETTI":["MAYBE TOMORROW","MAYBE TOMORROW!"]
+    ]
+//        "doododoododo","FOUR SHIFTER GEAR BOX":"ONE TWO THREE FOUR","A BOWL OF SPAGHETTI":"MAYBE TOMORROW"]"GET ALONG LITTLE DOGGIE"
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "YEEEEEE", message:"HAWWWWW", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "YEE HAW MATE", style: UIAlertAction.Style.default, handler: nil))
+        let random:UInt32 = arc4random_uniform(2)
+        let selectedEmoji = sender.titleLabel?.text
+        let emojiMessage = customMessage[Emoji[selectedEmoji!]!]?[Int(random)]
+        let alertController = UIAlertController(title: Emoji[selectedEmoji!], message:emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "next", style: UIAlertAction.Style.default, handler: nil))
    
         present(alertController, animated: true, completion: nil)
   
