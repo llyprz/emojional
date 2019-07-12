1a3dd121f5b492f6db5de6299a8c1904569ac2de
Apple <apple@Apples-MacBook-Pro-7.local>
2019-07-12 11:18:07 -0500

    Iteration 2

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "YEEEEEE", message:"HAWWWWW", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "YEE HAW MATE", style: UIAlertAction.Style.default, handler: nil))
+   
+        present(alertController, animated: true, completion: nil)
+  
+    }
 }
-
